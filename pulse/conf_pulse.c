5d1c1b862603937bc28a66c58131b7bebc11e8c6
Sjoerd Simons <sjoerd@luon.net>
2008-04-30 21:18:15 +0200

    Pulseaudio alsa configure hook
    The attached patch extends the alsa pulse plugin set with a alsa
    configuration hook. Allowing one to specify some configuration parameters
    that only come into effect when pulseaudio is running.
    
    For example a configution file like:
    
        @hooks [ {
           func on_pulse_is_running
             pcm.!default { type pulse }
             ctl.!default { type pulse }
           }
       ]
    
    will redirect the default alsa pcm and ctl to pulse iff pulse is running.
    (Assuming you defined the hook function correctly ofcourse)
    
    This is usefull for distributions that don't want to force their users to
    switch completely to pulseaudio, but have things a bit more dynamic :)
    
    The solutions isn't optimal though. It will mean that every program loading
    accessing alsa will try to make an (extra) connection to pulse to decide what
    to do. But i think it's the best we can do for now (or at least that i can do
    with my minimal knowledge of alsa).
    
    A nicer solution would be a way to always specify the pulse plugin as default
    and have a sort of fallback for when that fails.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

---
+++ pulse/conf_pulse.c
@@ -0,0 +1,88 @@
+/*
+ * ALSA configuration function extensions for pulse
+ *
+ * Copyright (c) 2008 by Sjoerd Simons <sjoerd@luon.net>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ *
+ */
+
+#include <stdio.h>
+
+#include <alsa/asoundlib.h>
+#include <pulse/pulseaudio.h>
+
+
+int
+conf_pulse_hook_on_is_running (snd_config_t *root, snd_config_t *config,
+    snd_config_t **dst, snd_config_t *private_data) {
+  snd_config_t *n = NULL;
+  pa_mainloop *loop = NULL;
+  pa_context *context = NULL;
+  int ret = 0, err, state;
+
+
+  /* Defined if we're called inside the pulsedaemon itself */
+  if (getenv("PULSE_INTERNAL") != NULL)
+    goto out;
+
+  loop = pa_mainloop_new();
+  if (loop == NULL)
+    goto out;
+
+  context = pa_context_new(pa_mainloop_get_api(loop), "Alsa hook");
+  if (context == NULL)
+    goto out;
+
+  err = pa_context_connect (context, NULL, 0, NULL);
+  if (err < 0)
+    goto out;
+
+  do {
+    err = pa_mainloop_prepare (loop, -1);
+    if (err < 0)
+       goto out;
+
+    err = pa_mainloop_poll (loop);
+    if (err < 0)
+      goto out;
+
+    err = pa_mainloop_dispatch (loop);
+    if (err < 0)
+      goto out;
+
+    state = pa_context_get_state(context);
+  } while (state < PA_CONTEXT_READY);
+
+  if (state != PA_CONTEXT_READY)
+    goto out;
+
+  ret = snd_config_expand(config, root, NULL, private_data, &n);
+
+out:
+  if (context != NULL)
+    pa_context_unref(context);
+
+  if (loop != NULL)
+    pa_mainloop_free(loop);
+
+  *dst = n;
+
+  return ret;
+}
+
+SND_DLSYM_BUILD_VERSION(conf_pulse_hook_on_is_running,
+  SND_CONFIG_DLSYM_VERSION_HOOK);
