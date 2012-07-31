81ae188c8d1b42d187b97846e3b31479a00d4720
Takashi Iwai <tiwai@suse.de>
2012-07-31 11:18:54 +0200

    usb_stream: Fix build with glibc 2.16
    _GNU_SOURCE needs to be defined at first.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- usb_stream/pcm_usb_stream.c
+++ usb_stream/pcm_usb_stream.c
@@ -18,8 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 
-#include <byteswap.h>
 #define _GNU_SOURCE
+#include <byteswap.h>
 #include <sys/mman.h>
 #include <sys/shm.h>
 #include <sys/ioctl.h>
