f576eaedf18c279357654f071678e873bce14727
Takashi Iwai <tiwai@suse.de>
2015-03-12 09:13:44 +0100

    rate: Allow LGPL for libsamplerate commercial licensee
    alsa-plugins-rate is tagged with GPL just for following the license of
    libsamplerate.  However, libsamplerate provides a commercial license
    to allow it used with less restrictions.  In that case, we also don't
    have to stick with GPL.  Instead, the code follows LGPL like other
    plugin codes in this package.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- rate/rate_samplerate.c
+++ rate/rate_samplerate.c
@@ -3,6 +3,14 @@
  *
  * Copyright (c) 2006 by Takashi Iwai <tiwai@suse.de>
  *
+ * This plugin code is supposed to be used and distributed primarily
+ * under GPL v2 or later, in order to follow the license of libsamplerate.
+ * However, if you already own a commercial license to use libsamplerate
+ * for dynamic linking, this plugin code can be used and distributed also
+ * under LGPL v2.1 or later.
+ *
+ * For GPL-2.0+:
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -13,9 +21,17 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ * For LGPL-2.1+:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 
 #include <stdio.h>
