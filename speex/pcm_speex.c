4e98854b590db9fd8faab0fea262277805bc7769
Takashi Iwai <tiwai@suse.de>
2015-11-09 11:53:22 +0100

    speex: Add missing include config.h
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- speex/pcm_speex.c
+++ speex/pcm_speex.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 
+#include "config.h"
 #include <alsa/asoundlib.h>
 #include <alsa/pcm_external.h>
 #include <speex/speex_preprocess.h>
