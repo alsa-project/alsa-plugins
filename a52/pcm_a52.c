85ead3367ab86198a98190aa4869e59b1fc69c2f
Anatol Pomozov <anatol.pomozov@gmail.com>
2016-02-15 17:49:22 -0800

    a52: Make compatible with ffmpeg 3.0
    audioconvert.h has been deprecated a while ago and got removed in
    ffmpeg 3.0 (commit 2d40968dd3ff17b12f7).
    
    Signed-off-by: Anatol Pomozov <anatol.pomozov@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- a52/pcm_a52.c
+++ a52/pcm_a52.c
@@ -39,7 +39,7 @@
 #endif
 
 #if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(53, 34, 0)
-#include <libavutil/audioconvert.h>
+#include <libavutil/channel_layout.h>
 #include <libavutil/mem.h>
 #define USE_AVCODEC_FRAME
 #endif
