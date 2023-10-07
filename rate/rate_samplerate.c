873a2966d22cd2956dc3c844fdc5d5573178502a
Takashi Iwai <tiwai@suse.de>
2023-10-07 09:20:41 +0200

    samplerate: Make LGPL-only
    As libsamplerate itself was already relicensed to 2-Clause BSD
    license, there is no need for restriction of GPL license for
    samplerate plugin, too.
    
    Let's switch to LGPL-only for relaxing the use.
    
    Luckily, I am the sole author of this piece of code, so it should be
    no legal problem for immediate relicensing.
    
    Closes: https://github.com/alsa-project/alsa-plugins/issues/54
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- rate/rate_samplerate.c
+++ rate/rate_samplerate.c
@@ -3,26 +3,6 @@
  *
  * Copyright (c) 2006 by Takashi Iwai <tiwai@suse.de>
  *
- * This plugin code is supposed to be used and distributed primarily
- * under GPL v2 or later, in order to follow the license of libsamplerate.
- * However, if you already own a commercial license to use libsamplerate
- * for dynamic linking, this plugin code can be used and distributed also
- * under LGPL v2.1 or later.
- *
- * For GPL-2.0+:
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * For LGPL-2.1+:
- *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
